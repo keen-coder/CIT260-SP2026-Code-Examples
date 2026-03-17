4568d5ba8eff1d73a8932e3c9d0c92b22b1796e3
keen-coder <kknaur@gmail.com>
2026-03-17 14:00:01 -0400

    Updated circle class

--- week08_&_09_sets_object_io_oop/object__oriented_programming/circle_class/circle.py
+++ week08_&_09_sets_object_io_oop/object__oriented_programming/circle_class/circle.py
@@ -3,13 +3,18 @@ import math
 class Circle:
 
     def __init__(self, radius=1.0):
-        self.__radius = radius
-
+        #self.__radius = radius
+        self.set_radius(radius)
+   
     def get_radius(self):
         return self.__radius
     
-    def set_radius(self, radius):
-        self.__radius = radius
+    def set_radius(self, radius=1.0):
+        if radius < 0:
+            print('Error: Radius cannot be negative')
+            print('Radius not changed.')
+        else:
+            self.__radius = radius
 
     def area(self):
         return self.__radius**2 * math.pi
